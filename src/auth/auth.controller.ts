8abbc30f8bb0501d58b5850b8c6c928386da3053
WilliammCorreia <william.correia@edu.ece.fr>
2025-01-29 21:10:06 +0100

    guard functionality

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
+import { AuthGuard } from "./auth.guard";
 
 @Controller('auth')
 export class AuthController {
@@ -18,5 +19,12 @@ export class AuthController {
     signin(@Body() dto: AuthDto) {
         console.log("dto : ", dto);
         return this.authService.signin(dto);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+        console.log(req.user);
+        return req.user;
     }
 }
